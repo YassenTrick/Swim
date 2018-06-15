535ac70a41a4e7c00149aefa93fac401a353d48c
iiFlamiinBlaze <iiflamiinblaze123@gmail.com>
2018-06-15 08:18:13 -0400

    Cleanup

--- src/Swim/Main.php
+++ src/Swim/Main.php
@@ -15,92 +15,76 @@ use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\network\mcpe\protocol\PlayerActionPacket;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\TextFormat as C;
+use pocketmine\utils\TextFormat;
 
-class Main extends PluginBase implements Listener {
+class Main extends PluginBase implements Listener{
 
-    public const PREFIX =  C::GREEN . "CLADevs " . C::RESET;
+	public const PREFIX = TextFormat::GREEN . "CLASwim " . TextFormat::RESET;
+	public const STOP_ACTION = 0;
+	public const START_ACTION = 1;
 
-    public const STOP_ACTION = 0;
-
-    public const START_ACTION = 1;
-
-	public function onEnable(): void{
-	    $this->getServer()->getPluginManager()->registerEvents($this, $this);
-	    $this->getLogger()->info(self::PREFIX . "Enabled");
+	public function onEnable() : void{
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
-	public function onDisable()
-    {
-        $this->getLogger()->info(self::PREFIX . "Disabled");
-    }
 
-    /**
-     * @param PlayerMoveEvent $event
-     */
-    public function onMove(PlayerMoveEvent $event): void{
-        $player = $event->getPlayer();
+	/**
+	 * @param PlayerMoveEvent $event
+	 * @return void
+	 */
+	public function onMove(PlayerMoveEvent $event) : void{
+		$player = $event->getPlayer();
+		if($this->inWater($player)){
+			if($player->isSprinting()){
+				$this->send($player, self::START_ACTION);
+				$player->exhaust(0.015, PlayerExhaustEvent::CAUSE_SWIMMING);
+				$player->addEffect(new EffectInstance(Effect::getEffect(Effect::NIGHT_VISION), 10, 0, false));
+			}else{
+				$this->send($player, self::START_ACTION);
+				$player->exhaust(0.015, PlayerExhaustEvent::CAUSE_SWIMMING);
+				$player->addEffect(new EffectInstance(Effect::getEffect(Effect::NIGHT_VISION), 10, 0, false));
+			}
+		}else{
+			$this->send($player, self::STOP_ACTION);
+			$player->removeEffect(Effect::NIGHT_VISION);
+		}
+	}
 
-        if($this->inWater($player)){
-            if($player->isSprinting()){
+	/**
+	 * @param Player $player
+	 * @param int    $action
+	 * @return void
+	 */
+	public function send(Player $player, int $action) : void{
+		switch($action){
+			case self::STOP_ACTION:
+				$pk = new PlayerActionPacket();
+				$pk->entityRuntimeId = Entity::$entityCount++;
+				$pk->x = $player->getFloorX();
+				$pk->y = $player->getFloorY();
+				$pk->z = $player->getFloorZ();
+				$pk->face = $player->getDirection();
+				$pk->action = $pk::ACTION_STOP_SWIMMING;
+				$player->setGenericFlag($player::DATA_FLAG_SWIMMING, false);
+				return;
+			case self::START_ACTION:
+				$pk = new PlayerActionPacket();
+				$pk->entityRuntimeId = Entity::$entityCount++;
+				$pk->x = $player->getFloorX();
+				$pk->y = $player->getFloorY();
+				$pk->z = $player->getFloorZ();
+				$pk->face = $player->getDirection();
+				$pk->action = $pk::ACTION_START_SWIMMING;
+				$player->setGenericFlag($player::DATA_FLAG_SWIMMING, true);
+				return;
+		}
+		$this->getServer()->broadcastPacket($this->getServer()->getOnlinePlayers(), $pk);
+	}
 
-                $this->send($player, self::START_ACTION);
-                $player->exhaust(0.015, PlayerExhaustEvent::CAUSE_SWIMMING);
-                $player->addEffect(new EffectInstance(Effect::getEffect(Effect::NIGHT_VISION), 10, 0, false));
-            } else {
-                $this->send($player, self::START_ACTION);
-                $player->exhaust(0.015, PlayerExhaustEvent::CAUSE_SWIMMING);
-                $player->addEffect(new EffectInstance(Effect::getEffect(Effect::NIGHT_VISION), 10, 0, false));
-            }
-        } else {
-            $this->send($player, self::STOP_ACTION);
-            $player->removeEffect(Effect::NIGHT_VISION);
-        }
-    }
-
-    /**
-     * @param Player $player
-     * @param int $action
-     */
-    public function send(Player $player, int $action){
-
-        switch($action){
-
-            case self::STOP_ACTION:
-
-                $pk = new PlayerActionPacket();
-                $pk->entityRuntimeId = Entity::$entityCount++;
-                $pk->x = $player->getFloorX();
-                $pk->y = $player->getFloorY();
-                $pk->z = $player->getFloorZ();
-                $pk->face = $player->getDirection();
-                $pk->action = $pk::ACTION_STOP_SWIMMING;
-
-                $player->setGenericFlag($player::DATA_FLAG_SWIMMING, false);
-                break;
-
-            case self::START_ACTION:
-                $pk = new PlayerActionPacket();
-                $pk->entityRuntimeId = Entity::$entityCount++;
-                $pk->x = $player->getFloorX();
-                $pk->y = $player->getFloorY();
-                $pk->z = $player->getFloorZ();
-                $pk->face = $player->getDirection();
-                $pk->action = $pk::ACTION_START_SWIMMING;
-
-                $player->setGenericFlag($player::DATA_FLAG_SWIMMING, true);
-                break;
-        }
-
-        $this->getServer()->broadcastPacket($this->getServer()->getOnlinePlayers(), $pk);
-
-    }
-
-    /**
-     * @param Player $player
-     * @return bool
-     */
-    public function inWater(Player $player): bool{
-        return $player->getLevel()->getBlockAt($player->getFloorX(), $player->getFloorY() - 1, $player->getFloorZ()) instanceof Water || $player->getLevel()->getBlockAt($player->getFloorX(), $player->getFloorY() - 1, $player->getFloorZ()) instanceof StillWater;
-
-    }
+	/**
+	 * @param Player $player
+	 * @return bool
+	 */
+	public function inWater(Player $player) : bool{
+		return $player->getLevel()->getBlockAt($player->getFloorX(), $player->getFloorY() - 1, $player->getFloorZ()) instanceof Water || $player->getLevel()->getBlockAt($player->getFloorX(), $player->getFloorY() - 1, $player->getFloorZ()) instanceof StillWater;
+	}
 }
